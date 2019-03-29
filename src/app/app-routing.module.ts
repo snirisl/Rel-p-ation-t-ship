09150a2018cce30e5d171544df9f442a84c48f2a
Snir Israeli <snir.isl@gmail.com>
2019-03-29 16:10:14 +0300

    Working on App 29.3_2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'requests', pathMatch: 'full' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
-  { path: 'requests', loadChildren: './requests/requests.module#RequestsPageModule' },
-  { path: 'about', loadChildren: './about/about.module#AboutPageModule' },
+  { path: 'requests', loadChildren: './requests/requests.module#RequestsPageModule', canLoad: [AuthGuard] },
+  { path: 'about', loadChildren: './about/about.module#AboutPageModule', canLoad: [AuthGuard] },
 ];
 
 @NgModule({
