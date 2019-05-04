3ac66382227840ff24c18fe82eadb62069514ba5
Snir Israeli <snir.isl@gmail.com>
2019-05-04 17:29:17 +0300

    adding capacitor and android capability to store auth data in device storage

---
+++ www/auth-auth-module.js
@@ -0,0 +1,185 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["auth-auth-module"],{
+
+/***/ "./src/app/auth/auth.module.ts":
+/*!*************************************!*\
+  !*** ./src/app/auth/auth.module.ts ***!
+  \*************************************/
+/*! exports provided: AuthPageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AuthPageModule", function() { return AuthPageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
+/* harmony import */ var _auth_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./auth.page */ "./src/app/auth/auth.page.ts");
+
+
+
+
+
+
+
+var routes = [
+    {
+        path: '',
+        component: _auth_page__WEBPACK_IMPORTED_MODULE_6__["AuthPage"]
+    }
+];
+var AuthPageModule = /** @class */ (function () {
+    function AuthPageModule() {
+    }
+    AuthPageModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: [
+                _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"],
+                _ionic_angular__WEBPACK_IMPORTED_MODULE_5__["IonicModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_4__["RouterModule"].forChild(routes)
+            ],
+            declarations: [_auth_page__WEBPACK_IMPORTED_MODULE_6__["AuthPage"]]
+        })
+    ], AuthPageModule);
+    return AuthPageModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/auth/auth.page.html":
+/*!*************************************!*\
+  !*** ./src/app/auth/auth.page.html ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<ion-header>\n  <ion-toolbar>\n    <ion-title>{{ isLogin ? 'Login' : 'Signup' }}</ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content padding>\n  <form #f=\"ngForm\" (ngSubmit)=\"onSubmit(f)\">\n    <ion-grid>\n      <ion-row>\n        <ion-col size-sm=\"6\" offset-sm=\"3\">\n          <ion-list>\n            <ion-item>\n              <ion-label position=\"floating\">Patient / Nurse ID</ion-label>\n              <ion-input\n                type=\"number\"\n                ngModel\n                name=\"pid\"\n                required\n                #pidCtrl=\"ngModel\"\n              ></ion-input>\n            </ion-item>\n            <ion-item *ngIf=\"!pidCtrl.valid && pidCtrl.touched\" lines=\"none\">\n              <ion-label>\n                Should be at least 9 characters long.\n              </ion-label>\n            </ion-item>\n            <ion-item>\n              <ion-label position=\"floating\">Password</ion-label>\n              <ion-input\n                type=\"password\"\n                ngModel\n                name=\"password\"\n                required\n                minlength=\"9\"\n                #passwordCtrl=\"ngModel\"\n              ></ion-input>\n            </ion-item>\n            <ion-item\n              *ngIf=\"!passwordCtrl.valid && passwordCtrl.touched\"\n              lines=\"none\"\n            >\n              <ion-label>\n                Should be at least 9 characters long.\n              </ion-label>\n            </ion-item>\n          </ion-list>\n        </ion-col>\n      </ion-row>\n      <ion-row>\n        <ion-col size-sm=\"6\" offset-sm=\"3\">\n          <div text-center>\n            <ion-button\n              type=\"button\"\n              color=\"primary\"\n              fill=\"clear\"\n              expand=\"block\"\n              (click)=\"onSwitchAuthMode()\"\n            >\n              Switch to {{ isLogin ? 'Signup' : 'Login' }}\n            </ion-button>\n            <ion-button\n            type=\"submit\"\n            color=\"primary\"\n            expand=\"block\"\n            [disabled]=\"!f.valid\"\n          >\n            {{ isLogin ? 'Login' : 'Signup'}}\n          </ion-button>\n          </div>\n        </ion-col>\n      </ion-row>\n    </ion-grid>\n  </form>\n</ion-content>\n"
+
+/***/ }),
+
+/***/ "./src/app/auth/auth.page.scss":
+/*!*************************************!*\
+  !*** ./src/app/auth/auth.page.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2F1dGgvYXV0aC5wYWdlLnNjc3MifQ== */"
+
+/***/ }),
+
+/***/ "./src/app/auth/auth.page.ts":
+/*!***********************************!*\
+  !*** ./src/app/auth/auth.page.ts ***!
+  \***********************************/
+/*! exports provided: AuthPage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AuthPage", function() { return AuthPage; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _auth_service__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./auth.service */ "./src/app/auth/auth.service.ts");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
+
+
+
+
+
+var AuthPage = /** @class */ (function () {
+    function AuthPage(authService, router, loadingCtrl, alertCtrl) {
+        this.authService = authService;
+        this.router = router;
+        this.loadingCtrl = loadingCtrl;
+        this.alertCtrl = alertCtrl;
+        this.isLoading = false;
+        this.isLogin = true;
+    }
+    AuthPage.prototype.ngOnInit = function () { };
+    AuthPage.prototype.authenticate = function (email, password) {
+        var _this = this;
+        this.isLoading = true;
+        this.loadingCtrl
+            .create({
+            keyboardClose: true,
+            message: 'Logging in...'
+        })
+            .then(function (loadingEl) {
+            loadingEl.present();
+            var authObs;
+            if (_this.isLogin) {
+                authObs = _this.authService.login(email, password);
+            }
+            else {
+                authObs = _this.authService.signup(email, password);
+            }
+            authObs.subscribe(function (resData) {
+                console.log(resData);
+                _this.isLoading = false;
+                loadingEl.dismiss();
+                _this.router.navigateByUrl('/requests/tabs/add-requests');
+            }, function (errRes) {
+                loadingEl.dismiss();
+                var code = errRes.error.error.message;
+                var message = 'Could not sign you up, please try again.';
+                if (code === 'EMAIL_EXISTS') {
+                    message = 'This Id exists already!';
+                }
+                else if (code === 'EMAIL_NOT_FOUND') {
+                    message = 'No such user.';
+                }
+                else if (code === 'INVALID_PASSWORD') {
+                    message = 'Could not log you in, please try again.';
+                }
+                _this.showAlert(message);
+            });
+        });
+    };
+    AuthPage.prototype.onSubmit = function (form) {
+        if (!form.valid) {
+            return;
+        }
+        var uid = form.value.pid + '@test.com';
+        var password = form.value.password;
+        console.log(uid, password);
+        this.authenticate(uid, password);
+    };
+    AuthPage.prototype.showAlert = function (message) {
+        this.alertCtrl
+            .create({
+            header: 'Authentication failed',
+            message: message,
+            buttons: ['Okay']
+        })
+            .then(function (alertEl) { return alertEl.present(); });
+    };
+    AuthPage.prototype.onSwitchAuthMode = function () {
+        this.isLogin = !this.isLogin;
+    };
+    AuthPage = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-auth',
+            template: __webpack_require__(/*! ./auth.page.html */ "./src/app/auth/auth.page.html"),
+            styles: [__webpack_require__(/*! ./auth.page.scss */ "./src/app/auth/auth.page.scss")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_auth_service__WEBPACK_IMPORTED_MODULE_2__["AuthService"],
+            _angular_router__WEBPACK_IMPORTED_MODULE_3__["Router"],
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["LoadingController"],
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["AlertController"]])
+    ], AuthPage);
+    return AuthPage;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=auth-auth-module.js.map
