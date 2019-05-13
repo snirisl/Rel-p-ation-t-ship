73c93ad76fcb513d393b90bd13ce5b3ad211afcf
orelia1 <48382806+orelia1@users.noreply.github.com>
2019-05-13 13:55:25 +0300

    Add files via upload

--- OCR/download.js
+++ OCR/download.js
@@ -1,5 +1,5 @@
 let img = new Image()
-img.src= "pic.jpg"
+img.src= "pic.jpeg"
 img.onload =function(){ 
 
   Tesseract.recognize(img)
@@ -7,7 +7,7 @@ img.onload =function(){
     console.log('result is: ', result)
     $('#result').text(result.words.text);
 
-    var myObj = {name: result.words[1].text, lastName: result.words[0].text, birthDate: result.words[2].text, id: result.words[3].text};
+    var myObj = {name: result.words[0].text, lastName: result.words[1].text, id: result.words[2].text};
     var myJSON = JSON.stringify(myObj);
     var paitentObject= JSON.parse(myJSON);
     console.log(paitentObject);
